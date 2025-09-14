9d88f49398c494bec77abfd328c46e4df50c944f
qminhng05 <quangminh.nguyen2@students.mq.edu.au>
2025-09-15 01:11:47 +1000

    fix

--- app.py
+++ app.py
@@ -11,21 +11,14 @@ bitbucket_model = joblib.load("bitbucket_model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json
-    inputs = [
-        data['company_size_encoded'],
-        data['is_enterprise_encoded'],
-        data['industry_education'], data['industry_energy'], data['industry_finance'], data['industry_healthcare'],
-        data['industry_manufacturing'], data['industry_media'], data['industry_retail'], data['industry_software'],
-        data['industry_telecom'], data['industry_transport'],
-        data['device_desktop'], data['device_mobile'], data['device_web'],
-        data['os_android'], data['os_ios'], data['os_linux'], data['os_mac'], data['os_win']
-    ]
+    data = request.get_json()
+    
+    features = data.get("features", [])
 
-    jira_result = jira_model.predict([inputs])[0]
-    confluence_result = confluence_model.predict([inputs])[0]
-    trello_result = trello_model.predict([inputs])[0]
-    bitbucket_result = bitbucket_model.predict([inputs])[0]
+    jira_result = jira_model.predict([features])[0]
+    confluence_result = confluence_model.predict([features])[0]
+    trello_result = trello_model.predict([features])[0]
+    bitbucket_result = bitbucket_model.predict([features])[0]
 
     return jsonify({
         "Jira": str(jira_result),
